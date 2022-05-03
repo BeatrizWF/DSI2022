537e0b756086a47485326afb91c788464ec4aff8
BEATRIZ WARMELING FERREIRA <beatriz.ferreira@univille.br>
2022-05-03 19:29:54 -0300

    first commit

---
+++ src/test/java/br/univille/petservices/PetservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.petservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
